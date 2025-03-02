0021f1f6ca78bedb32e82c478dccf75179df651d
hhoot25 <hhoot25@gmail.com>
2025-03-01 19:34:31 -0600

    added pages and started form

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const myLibrary = [{author: 'harry', pages:50, readStatus:true}, {author: 'bob', pages:45, readStatus:true}];
+const myLibrary = [{title:"Cat and the Hat",author: 'harry', pages:50, readStatus:true}, {title:"Story of My Life", author: 'bob', pages:45, readStatus:true}];
 const container = document.querySelector('#container');
 
-function Book(author,pages,readStatus){
+function Book(title, author,pages,readStatus){
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
@@ -16,10 +17,38 @@ function loopLibrary(){
     myLibrary.forEach(function(item){
         const book = document.createElement("div");
         book.className += " book";
-        book.innerHTML=item.author;
+
+        const title = document.createElement("div");
+        title.className += "title";
+        title.innerHTML = `Title: ${item.title}`;
+
+        const author = document.createElement("div");
+        author.className += "author";
+        author.innerHTML = `Author: ${item.author}`;
+
+        const pages = document.createElement("div");
+        pages.className += "pages";
+        pages.innerHTML = `Pages: ${item.pages}`;
+
+
+        const read = document.createElement("div");
+        read.className += "read";
+        read.innerHTML = `Read Status: ${item.readStatus}`;
+
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(read);
 
         container.appendChild(book);
     });
+
+    const form = document.createElement("div");
+    form.id = "form";
+    form.innerHTML="Open Form";
+    container.appendChild(form);
+
+
 }
 
 loopLibrary();
